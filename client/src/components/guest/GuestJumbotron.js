3d452d47ee67cc139799e9f45c143c02081edf69
Deleon Francis <dxfrancis.coding@gmail.com>
2021-04-25 12:38:37 -0400

    created guestModals dir and moved the guest add not modal component.

--- client/src/components/guest/GuestJumbotron.js
+++ client/src/components/guest/GuestJumbotron.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import FilterNotes from "../notes/FilterNotes";
-import GuestAddNoteModal from "./GuestAddNoteModal";
+import GuestAddNoteModal from "../guestModals/GuestAddNoteModal";
 
 
 function GuestJumbotron() {
